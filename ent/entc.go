0ce0c4089a38eb7f5ae86825f30e52abe4b70450
mingqing <mingqing@henji.org>
2025-02-27 17:11:43 +0800

    refactor: 添加基础数据库表结构

--- ent/entc.go
+++ ent/entc.go
@@ -4,14 +4,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 )
 
 func main() {
-	if err := entc.Generate("./schema", &gen.Config{}); err != nil {
+	goPath := os.Getenv("GOPATH")
+
+	if err := entc.Generate("./schema",
+		&gen.Config{
+			Target:  fmt.Sprintf("%v/src/github.com/grpc-kit/pkg/lion", goPath),
+			Package: "github.com/grpc-kit/pkg/lion",
+		},
+	); err != nil {
 		log.Fatal("running ent codegen:", err)
 	}
 }
